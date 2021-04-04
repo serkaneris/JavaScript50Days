4da9842d2221173f5c124cd04504f4d116baa568
serkaneris <serkan_eris@hotmail.com>
2021-04-05 00:00:26 +0300

    index container bug fixed

--- Day01/script.js
+++ Day01/script.js
@@ -2,6 +2,7 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel=>{
     panel.addEventListener('click', ()=>{
+        console.log('panel clicked!')
         removeActiveClasses()
         panel.classList.add('active')
     })
